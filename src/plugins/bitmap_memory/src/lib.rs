fc5ea187d2a6dbcd2db2c1e941270abd205a7207
Daniel Collin <daniel@collin.com>
2015-12-26 11:31:50 +0100

    Added bitmap_memory view plugin in Rust

---
+++ src/plugins/bitmap_memory/src/lib.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+extern crate prodbg;
+
+use prodbg::*;
+
+struct BitmapView {
+    dummy: i32,
+}
+
+impl View for BitmapView {
+    fn new(_: &Ui, _: &Service) -> Self {
+        BitmapView { 
+            dummy: 0 
+        }
+    }
+
+    fn update(&mut self, ui: &Ui, _: &mut Reader, _: &mut Writer) {
+        ui.button("test");
+        self.dummy += 1;
+    }
+}
+
+#[no_mangle]
+#[allow(unused_mut)] // likely due to compiler bug
+pub fn init_plugin(plugin_handler: &mut PluginHandler) {
+    println!("R: init_plugin");
+    let mut plugin = define_view_plugin!(BitmapView);
+    plugin_handler.register_view(VIEW_API_VERSION, &mut plugin);
+}
