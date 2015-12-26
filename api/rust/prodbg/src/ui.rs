ebae37d392b86e991b4b1092216eb4550927c814
Daniel Collin <daniel@collin.com>
2015-12-26 10:12:54 +0100

    Added unit-tests for StringHandler
    We should likely move these to a separte file instead
    
    Also removed all Optional<> in ui_ffi interface as these aren't optional anyway

--- api/rust/prodbg/src/ui.rs
+++ api/rust/prodbg/src/ui.rs
@@ -1,4 +1,4 @@
-use std::ffi::CString;
+use std::ffi::{CString, CStr};
 use std::mem;
 use std::ptr;
 use ui_ffi::*;
@@ -7,7 +7,7 @@ pub struct Ui {
     pub api: *mut CPdUI,
 }
 
-const STRING_SIZE: usize = 1024;
+const STRING_SIZE: usize = 512;
 
 struct StringHandler {
     pub local: bool,
@@ -16,23 +16,25 @@ struct StringHandler {
 }
 
 impl StringHandler {
-    pub unsafe fn new(name: &str) -> StringHandler {
-        let name_len = name.len();
-        if name_len < STRING_SIZE - 1 {
-            let mut handler = StringHandler {
-                local: true,
-                local_string: mem::uninitialized(),
-                heap_string: mem::uninitialized(),
-            };
+    pub fn new(name: &str) -> StringHandler {
+        unsafe {
+            let name_len = name.len();
+            if name_len <= STRING_SIZE - 1 {
+                let mut handler = StringHandler {
+                    local: true,
+                    local_string: mem::uninitialized(),
+                    heap_string: mem::uninitialized(),
+                };
 
-            ptr::copy(name.as_ptr(), handler.local_string.as_mut_ptr(), name_len);
-            handler.local_string[name_len] = 0;
-            handler
-        } else {
-            StringHandler {
-                local: false,
-                local_string: mem::uninitialized(),
-                heap_string: CString::new(name).unwrap(),
+                ptr::copy(name.as_ptr(), handler.local_string.as_mut_ptr(), name_len);
+                handler.local_string[name_len] = 0;
+                handler
+            } else {
+                StringHandler {
+                    local: false,
+                    local_string: mem::uninitialized(),
+                    heap_string: CString::new(name).unwrap(),
+                }
             }
         }
     }
@@ -51,7 +53,57 @@ impl Ui {
         unsafe {
             let t = StringHandler::new(title);
             let s = PDVec2 { x: 0.0, y: 0.0 };
-            ((*self.api).button).unwrap()(t.as_ptr(), s);
+            ((*self.api).button)(t.as_ptr(), s);
         }
     }
 }
+
+
+// Only used in tests
+#[allow(dead_code)]
+fn get_string(handler: &StringHandler) -> String {
+    unsafe {
+        CStr::from_ptr(handler.as_ptr() as *const i8).to_string_lossy().into_owned()
+    }
+}
+
+#[test]
+fn test_string_handler() {
+    {
+        let short_string = "test_local";
+        let t = StringHandler::new(short_string);
+        assert_eq!(t.local, true);
+        assert_eq!(get_string(&t), short_string);
+    }
+    {
+        let short_string = "test_local stoheusthsotheost";
+        let t = StringHandler::new(short_string);
+        assert_eq!(t.local, true);
+        assert_eq!(get_string(&t), short_string);
+    }
+    {
+        // this string (with 511) buffer should just fit
+        let test_511_string = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooacd";
+        let t = StringHandler::new(test_511_string);
+        assert_eq!(t.local, true);
+        assert_eq!(get_string(&t), test_511_string);
+    }
+    {
+        // this string (with 512) buffer should no fit 
+        let test_512_string = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooabcd";
+        let t = StringHandler::new(test_512_string);
+        assert_eq!(t.local, false);
+        assert_eq!(get_string(&t), test_512_string);
+    }
+    {
+        // this string (with 513) buffer should no fit 
+        let test_513_string = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuueeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuoooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooabcd";
+        let t = StringHandler::new(test_513_string);
+        assert_eq!(t.local, false);
+        assert_eq!(get_string(&t), test_513_string);
+    }
+}
+
+
+
+
