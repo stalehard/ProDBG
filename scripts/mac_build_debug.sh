6c9ad25834a39d015c253436e381833ff8518233
Daniel Collin <daniel@collin.com>
2015-12-27 09:51:49 +0100

    Somewhat temporary building of Rust code

--- scripts/mac_build_debug.sh
+++ scripts/mac_build_debug.sh
@@ -1,3 +1,9 @@
 #!/bin/bash 
 bin/macosx/tundra/tundra2 -v macosx-clang-debug
 cargo build
+cd src/plugins/bitmap_memory
+cargo build
+cd ../../prodbg/tests/rust_api_test
+cargo build
+cd ../../../../
+
