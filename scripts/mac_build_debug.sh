70fb243d286a5b7b400f871ea5c1a9f909dc0037
Daniel Collin <daniel@collin.com>
2016-01-22 13:59:25 +0100

    Removed build of ui_bench for script

--- scripts/mac_build_debug.sh
+++ scripts/mac_build_debug.sh
@@ -3,8 +3,8 @@ bin/macosx/tundra/tundra2 -v macosx-clang-debug
 # cargo build
 # cargo build --manifest-path=src/plugins/bitmap_memory/Cargo.toml
 # cargo build --manifest-path=src/tests/rust_api_test/Cargo.toml
-cargo build --manifest-path=src/prodbg/ui_testbench/Cargo.toml
+# cargo build --manifest-path=src/prodbg/ui_testbench/Cargo.toml
 # cargo build --manifest-path=src/prodbg/prodbg/Cargo.toml
-cp target/debug/ui_testbench t2-output/macosx-clang-debug-default/ProDBG.app/Contents/MacOS
+# cp target/debug/ui_testbench t2-output/macosx-clang-debug-default/ProDBG.app/Contents/MacOS
 # cp target/debug/prodbg t2-output/macosx-clang-debug-default/ProDBG.app/Contents/MacOS
 
