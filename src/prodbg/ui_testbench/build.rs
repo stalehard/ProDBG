5ebaf152f19aa6373af16e96bbdf405faf32333e
Daniel Collin <daniel@collin.com>
2016-01-22 13:59:44 +0100

    Removed stuff, added tundra stuff

--- src/prodbg/ui_testbench/build.rs
+++ src/prodbg/ui_testbench/build.rs
@@ -1,55 +1,20 @@
 use std::env;
-use std::process::Command;
 
 fn main() {
-    let target = env::var("TARGET").unwrap();
-    let debug = env::var("DEBUG").unwrap();
-    let command;
-    let tundra;
+    let tundra_dir = env::var("TUNDRA_OBJECTDIR").unwrap_or("".to_string());
+    let libs = env::var("TUNDRA_STATIC_LIBS").unwrap_or("".to_string());
 
-    if target.contains("darwin") {
-        tundra = "../../../bin/macosx/tundra/tundra2";
-        if debug == "true" {
-            command = "macosx-clang-debug";
-        } else {
-            command = "macosx-clang-release";
-        }
-    } else if target.contains("windows") {
-        tundra = "../../bin/win32/tundra2";
-        if debug == "true" {
-            command = "win64-msvc-debug";
-        } else {
-            command = "win64-msvc-release";
-        }
-    } else {
-        tundra = "tundra2";
-        if debug == "true" {
-            command = "linux-gcc-debug";
-        } else {
-            command = "linux-gcc-release";
-        }
-    }
+    let native_libs = libs.split(" ");
 
-    let _ = Command::new(tundra)
-                .arg("-v")
-                .arg(command)
-                .output()
-                .unwrap_or_else(|e| panic!("Unable to execute tundra: {}", e));
+    println!("cargo:rustc-link-search=native={}", tundra_dir);
 
-    // Lib path
-    println!("cargo:rustc-link-search=native=t2-output/{}-default",
-             command);
-
-    // Ugly but hey!
-    let libs_to_link = ["ui", "lua", "remote_api", "stb", "bgfx",
-                        "imgui", "scintilla", "tinyxml2", "capstone"];
-
-    for lib in libs_to_link.iter() {
-        println!("cargo:rustc-link-lib=static={}", *lib);
+    for lib in native_libs {
+        println!("cargo:rustc-link-lib=static={}", lib);
+        println!("cargo:rerun-if-changed={}", lib);
     }
 
     println!("cargo:rustc-flags=-l dylib=stdc++");
     println!("cargo:rustc-flags=-l framework=Cocoa");
-
-    //println!("cargo:rustc-link-lib=static=stdc++");
 }
+
+
