e63e84e8b45ebcd0297db87a4433e4e4afcf6692
Daniel Collin <daniel@collin.com>
2016-04-23 22:21:06 +0200

    Load dummy backend plugin
    Also made some cleanups

--- src/prodbg/main/src/main.rs
+++ src/prodbg/main/src/main.rs
@@ -6,9 +6,9 @@ extern crate bgfx_rs;
 extern crate imgui_sys;
 
 pub mod windows;
-mod docking;
+// mod docking;
 
-use docking::DockingPlugin;
+//use docking::DockingPlugin;
 //use prodbg_api::ui::Ui;
 use bgfx_rs::Bgfx;
 //use imgui_sys::Imgui;
@@ -17,6 +17,7 @@ use core::session::Sessions;
 use windows::Windows;
 use core::{DynamicReload, Search};
 use core::view_plugins::{ViewPlugins};
+use core::backend_plugin::{BackendPlugins};
 use std::cell::RefCell;
 use std::rc::Rc;
 
@@ -44,19 +45,15 @@ fn main() {
     let mut lib_handler = DynamicReload::new(None, Some("t2-output"), Search::Backwards);
     let mut plugins = Plugins::new();
 
-    //let menus = create_menus();
-
-    // Would be nice to nat have it this way
     let view_plugins = Rc::new(RefCell::new(ViewPlugins::new()));
-    let docking_plugin = Rc::new(RefCell::new(DockingPlugin::new()));
-    //let backend_plugins = Rc::new(RefCell::new(BackendPlugins::new()));
+    let backend_plugins = Rc::new(RefCell::new(BackendPlugins::new()));
 
     sessions.create_instance();
 
     plugins.add_handler(&view_plugins);
-    plugins.add_handler(&docking_plugin);
-    //plugins.add_handler(&backend_plugins);
+    plugins.add_handler(&backend_plugins);
 
+    plugins.add_plugin(&mut lib_handler, "dummy_backend_plugin");
     plugins.add_plugin(&mut lib_handler, "bitmap_memory");
     plugins.add_plugin(&mut lib_handler, "registers_plugin");
     plugins.add_plugin(&mut lib_handler, "hex_memory_plugin");
